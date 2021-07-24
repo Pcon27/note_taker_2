6e736807a87919d0a432e7662c8fc6413dd3e5a5
pconsag <pconsagra@g.ucla.edu>
2021-07-23 17:56:01 -0700

    restructured files

---
+++ db/store.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const util = require('util');
+
+
+
+
+const readFromFile = util.promisify(fs.readFile);
+/**
+ *  
+ *  @param {string} destination 
+ *  @param {object} content 
+ *  @returns {void} 
+ */
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+/**
+ *  
+ *  @param {object} content 
+ *  @param {string} file 
+ *  @returns {void} 
+ */
+const readAndAppend = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+
+class Store {
+  read() {
+    return readFileAsync('db/db.json', 'utf8');
+  }
+
+  write(note) {
+    return writeFileAsync('db/db.json', JSON.stringify(note));
+  }
+
+  getNotes() {
+    return this.read().then((notes) => {
+      let parsedNotes;
+
+      // If notes isn't an array or can't be turned into one, send back a new empty array
+      try {
+        parsedNotes = [].concat(JSON.parse(notes));
+      } catch (err) {
+        parsedNotes = [];
+      }
+
+      return parsedNotes;
+    });
+  }}
+
+module.exports = { readFromFile, writeToFile, readAndAppend };
+
+
